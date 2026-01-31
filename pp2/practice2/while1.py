6e5ab49f8c9842ac7ea8a5c9b5009253fe819874
nabdilmanapov-lang <nabdilmanapov@gmail.com>
2026-01-31 18:01:24 +0500

    Update while1.py

--- pp2/practice2/while1.py
+++ pp2/practice2/while1.py
@@ -1,1 +1,25 @@
 
+#while the basic 
+n = int(input())
+i = 1
+s = 0
+while i <= n:
+    if i % 2 == 0:
+        s += i
+    else:
+        s += 0
+    i += 1
+print(s)
+if s > 0:
+    print("positive")
+else:
+    print("zero")
+print("done")
+
+#some basic example
+
+#enter 1 to 10
+x=1
+while x<=10:
+    print(x,end=" ")
+    x+=1
