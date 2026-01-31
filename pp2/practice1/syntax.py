44448755cfb425b91d36f9df5c7806a77ff24aee
nabdilmanapov-lang <nabdilmanapov@gmail.com>
2026-01-31 17:59:05 +0500

    Add files via upload

---
+++ pp2/practice1/syntax.py
@@ -0,0 +1,34 @@
+# syntax.py
+# Practice1 - Python Syntax examples
+#My anemis Azim and i am 18years old
+# --- Sub theme: Syntax (Indentation) ---
+if 5 > 2:
+    print("Five is greater than two!")
+
+# Примеры ошибок (закомментированы, чтобы файл запускался)
+"""
+if 5 > 2:
+print("Five is greater than two!")  # ERROR: no indentation
+"""
+
+"""
+if 5 > 2:
+    print("Five is greater than two!")
+        print("Five is greater than two!")  # ERROR: unexpected indent
+"""
+
+print("-----")
+
+# --- Sub theme: Statements ---
+print("Python is fun!")
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("-----")
+
+# Можно писать несколько команд в одной строке через ;
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+# Пример ошибки (тоже закомментирован)
+# print("Python is fun!") print("Really!")  # ERROR
