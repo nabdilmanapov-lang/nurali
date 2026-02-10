9e86be8e36f8d73316995f295a653c667ea30b84
nabdilmanapov-lang <nabdilmanapov@gmail.com>
2026-02-10 10:05:07 +0500

    Create basic_function.py

---
+++ pp2/practice3/functions/basic_function.py
@@ -0,0 +1,19 @@
+def KBTU():
+  print("Hello from KBTU")
+
+KBTU()
+  
+def  Almaty():
+  print("Hello from Almaty")
+
+Almaty()
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def cout():
+  print("Hello")
+
+cout()
