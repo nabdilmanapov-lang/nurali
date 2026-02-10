eaff4ff32724be87f18dc0340af3d1556f40c0e4
nabdilmanapov-lang <nabdilmanapov@gmail.com>
2026-02-10 10:04:08 +0500

    Create class_definition.py

---
+++ pp2/practice3/classes/class_definition.py
@@ -0,0 +1,22 @@
+class student:
+    name="Akylbek"
+    age=18
+    major="Automation"
+
+x=student()
+print(x.name)
+del x
+x=student()
+print(x.name)
+print(x.age)
+print(x.major)
+
+class football:
+    goal="Ronaldo"
+    assist="Messi"
+    pen="Mbappe"
+
+x=football()
+print(x.goal)
+print(x.assist)
+print(x.pen)
