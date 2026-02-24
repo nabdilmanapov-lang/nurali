d82a822d975f1f87d9f3c0a98790b525ded551ac
nabdilmanapov-lang <nabdilmanapov@gmail.com>
2026-02-24 14:10:34 +0500

    Create math.py

---
+++ pp2/practice4/math/math.py
@@ -0,0 +1,27 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+
+#The abs() function returns the absolute (positive) value of the specified number:
+x = abs(-7.25)
+print(x)
+
+#Return the value of 4 to the power of 3 (same as 4 * 4 * 4):
+x = pow(4, 3)
+print(x)
+
+#Python has also a built-in module called math, which extends the list of mathematical functions.To use it, you must import the math module:
+import math
+#The math.sqrt() method for example, returns the square root of a number:
+x = math.sqrt(64)
+print(x)
+y = math.ceil(1.4)
+z = math.floor(1.4)
+print(y) # returns 2
+print(z) # returns 1
+
+#The math.pi constant, returns the value of PI (3.14...):
+import math
+x = math.pi
+print(x)
